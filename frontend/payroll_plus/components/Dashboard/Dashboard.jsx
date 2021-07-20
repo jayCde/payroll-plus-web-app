d5eeda430b640b0e9fa5f62a58d5b6730a255b23
jayCde <68590879+jayCde@users.noreply.github.com>
2021-07-20 14:23:09 +0000

    Add skeleton for Dashboard

---
+++ frontend/payroll_plus/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navigation from './_navigation';
+import Body from './_body';
+import Footer from './_footer';
+
+
+const UserDashboard = () => {
+    return (
+        <div>
+            <>
+                Dashboard
+            </>
+        </div>
+    )
+}
+
+export default UserDashboard
