ac6ee46326220fbafc437bc4cf4bdb3867637460
jayCde <68590879+jayCde@users.noreply.github.com>
2021-07-20 19:39:26 +0000

    Import Head, render page title

--- frontend/payroll_plus/pages/dashboard/index.js
+++ frontend/payroll_plus/pages/dashboard/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Head from 'next/head'
 import UserDashboard from '../../components/Dashboard/Dashboard';
 
 const UserDashboardPage = () => {
     return (
         <div>
+            <Head>
+                <title>Dashboard</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <UserDashboard/>
         </div>
     )
