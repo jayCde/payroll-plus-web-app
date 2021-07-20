65bc2ce0bf4036e879032a2223051d824be7a053
jayCde <68590879+jayCde@users.noreply.github.com>
2021-07-20 14:25:15 +0000

    Add component skeletons

---
+++ frontend/payroll_plus/pages/auth/login/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserLogin from '../../../components/Authentication/login'
+
+const UserAuthentication = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UserAuthentication
---
+++ frontend/payroll_plus/pages/dashboard/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserDashboard from '../../components/Dashboard/Dashboard';
+
+const UserDashboardPage = () => {
+    return (
+        <div>
+            <UserDashboard/>
+        </div>
+    )
+}
+
+export default UserDashboardPage
