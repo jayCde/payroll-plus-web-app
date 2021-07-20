c4862d5620b459918746010470125df7e80f0445
jayCde <68590879+jayCde@users.noreply.github.com>
2021-07-20 14:22:34 +0000

    Add skeleton to dashboard components

---
+++ frontend/payroll_plus/components/Dashboard/_navigation.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from '../../styles/Dashboard/navigation.module.css'
+
+const Navigation = () => {
+    return (
+        <div>
+            <div className={styles.sidenaviagation}>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Navigation
