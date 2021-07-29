b6a984e50a8077806f8b6626c2a9f4431ad20bcc
jayCde <68590879+jayCde@users.noreply.github.com>
2021-07-29 21:32:21 +0000

    Modify UserAdmin auth fieldsets, add new seed

--- backend/api/payroll/admin.py
+++ backend/api/payroll/admin.py
@@ -25,7 +25,7 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide', ),
-            'fields': ('email', 'password', 'confirm_password'),
+            'fields': ('email', 'password1', 'password2'),
         }),
     )
 
