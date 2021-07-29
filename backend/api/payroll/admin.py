0d887a8eed19e46ff97dad7dcd54dd07f41d5144
jayCde <68590879+jayCde@users.noreply.github.com>
2021-07-29 17:11:17 +0000

    Add and register models

--- backend/api/payroll/admin.py
+++ backend/api/payroll/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Employees, Salary, Payroll
+
+admin.site.register(Employees)
+admin.site.register(Salary)
+admin.site.register(Payroll)
--- backend/api/payroll/models.py
+++ backend/api/payroll/models.py
@@ -19,21 +19,50 @@ class Employees(models.Model):
     employee_dob = models.DateField(null=False)
     employee_department = models.CharField(max_length=255, null=False)
     employee_unit = models.CharField(max_length=255, null=False)
-    employee_rate = models.IntegerField(max_length=255, null=False, default=0)
-    employee_baseSalary = models.IntegerField(max_length=None, null=False)
+    employee_rate = models.IntegerField(null=False, default=0)
+    employee_baseSalary = models.IntegerField(null=False, default=0)
+
+    #return full name from first and lastnames
+    @property
+    def employee_fullname(self):
+        return "%s  %s" % ( self.employee_firstname, self.employee_lastname)
+
+    def __str__(self):
+        return self.employee_fullname
+    
 
 #Salary Model
 class Salary(models.Model):
+    salary_id = models.IntegerField(primary_key=True, null=False)
     employee_id = models.ForeignKey(Employees, on_delete=models.CASCADE)
-    salary_amount = models.IntegerField(max_length=None, null=False, default=0)
-    amount_paid = models.IntegerField(max_length=None, null=False, default=0)
-    due_date = models.IntegerField(max_length=None, null=False)
-    date_paid = models.IntegerField(max_length=None, null=False)
+    salary_amount = models.IntegerField(null=False, default=0)
+    amount_paid = models.IntegerField(null=False, default=0)
+    due_date = models.IntegerField(null=False)
+    date_paid = models.IntegerField(null=False)
+
+    #return a stringed type of the salary id
+    @property
+    def sal_id(self):
+        return str(self.salary_id)
+
+    def __str__(self):
+        return self.sal_id
+    
 
 #Payroll Model
 
 class Payroll(models.Model):
+    payroll_id = models.IntegerField(primary_key=True, null=False)
     employee_id = models.ForeignKey(Employees, on_delete=models.CASCADE)
-    comphourlyRate = models.IntegerField(max_length=255, null=False, default=0)
-    daysClocked = models.IntegerField(max_length=255, null=False, default=0)
-    salary = models.IntegerField(max_length=255, null=False, default=0)
+    comphourlyRate = models.IntegerField(null=False, default=0)
+    daysClocked = models.IntegerField(null=False, default=0)
+    salary = models.IntegerField(null=False, default=0)
+
+#return a stringed type of the payroll id
+    @property
+    def emp_id(self):
+        return str(self.payroll_id)
+
+    def __str__(self):
+        return self.emp_id
+    
